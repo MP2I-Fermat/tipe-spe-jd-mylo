8fbf5266f6ae96c3a0cc22b896719e80ccd2159f
Jean Dubois <jd-dev@laposte.net>
2025-10-03 11:35:12 +0000

    fáùţêṡ d’àĉçèńţŭátîöñ

--- src/parser.ml
+++ src/parser.ml
@@ -20,14 +20,20 @@ type ('token_type, 'non_terminal) syntax_tree =
 type ('token_type, 'non_terminal) lr1_situation =
   ('token_type, 'non_terminal) rule * int * 'token_type list list
 
-(* Renvoie l’ensemble Premier_LR(1)(s) dans la grammaire g *)
-let premier_LR1 (s : ('token_type, 'non_terminal) grammar_entry list)
+(* Renvoie l’ensemble Premier_LL(1)(s) dans la grammaire g *)
+let premier_LL1 (s : ('token_type, 'non_terminal) grammar_entry list)
     (g : ('token_type, 'non_terminal) grammar) : 'token_type list =
+  (* Renvoie l’ensemble Premier_n_LL(1)(s) et un booléen qui indique si
+   * cet Premier_i_LL(1)(s) est constant à partir du rang n (cf. preuve). Le
+   * booléen n’est pas toujours à true à partir du premier rang, mais il sera
+   * forcément à true au bout d’un moment. *)
   let rec premier_n (n : int)
       (s : ('token_type, 'non_terminal) grammar_entry list) :
       'token_type list * bool =
     if n = 0 then
-      match s with [ Terminal a ] -> ([ a ], true) | _ -> ([], false)
+      match s with
+      | [ Terminal a ] -> ([ a ], true)
+      | _ -> ([], false)
     else
       match s with
       | [ NonTerminal a1 ] ->
@@ -55,7 +61,7 @@ let premier_LR1 (s : ('token_type, 'non_terminal) grammar_entry list)
               List.fold_left List.rev_append premier_nm1 premiers_derivations
               |> List.sort_uniq compare
             in
-            (* (premier_i(N)) est en fait constant a partir du rang n - 1, voir preuves/premier_n_cts.
+            (* (premier_i(N)) est en fait constant à partir du rang n - 1, voir preuves/premier_n_cts.
             On ne s'en rend compte que maintenant car on n'avait pas calculé premier_n des derivations possibles. *)
             ( prem_n,
               premiers_derivations_sont_constantes && prem_n = premier_nm1 )
@@ -67,9 +73,9 @@ let premier_LR1 (s : ('token_type, 'non_terminal) grammar_entry list)
             let prem_n =
               List.rev_append prem_nm1_a1 prem_nm1 |> List.sort_uniq compare
             in
-            (* premier_i(a1...am) est constant a partir du rang n - 1, voir preuves/premier_n_cts. *)
+            (* premier_i(a1...am) est constant à partir du rang n - 1, voir preuves/premier_n_cts. *)
             (prem_n, prem_a1_est_constant && prem_n = prem_nm1)
-      | [] -> failwith "Les règles N -> epsilon ne sont pas gérées"
+      | [] -> failwith "Les règles N -> ε ne sont pas gérées"
   in
   let rec trouve_premier_constant (n : int) =
     let premier_n, est_constant = premier_n n s in
@@ -77,12 +83,12 @@ let premier_LR1 (s : ('token_type, 'non_terminal) grammar_entry list)
   in
   trouve_premier_constant 0
 
-let test_premier_LR1 =
+let test_premier_LL1 =
   let grammaire1 (* page 123, exemple 144 *) =
     [ ('S', [ Terminal 'a'; NonTerminal 'T' ]); ('T', [ Terminal 'b' ]) ]
   in
-  assert (premier_LR1 [ Terminal 'a'; NonTerminal 'T' ] grammaire1 = [ 'a' ]);
-  assert (premier_LR1 [ NonTerminal 'T' ] grammaire1 = [ 'b' ]);
+  assert (premier_LL1 [ Terminal 'a'; NonTerminal 'T' ] grammaire1 = [ 'a' ]);
+  assert (premier_LL1 [ NonTerminal 'T' ] grammaire1 = [ 'b' ]);
   let grammaire2 (* page 60, exemple 24 *) =
     [
       ('S', [ NonTerminal 'U' ]);
@@ -91,11 +97,11 @@ let test_premier_LR1 =
       ('V', [ Terminal 'c'; Terminal 'd' ]);
     ]
   in
-  assert (premier_LR1 [ Terminal 'a'; Terminal 'b' ] grammaire2 = [ 'a' ]);
-  assert (premier_LR1 [ Terminal 'c'; Terminal 'd' ] grammaire2 = [ 'c' ]);
-  assert (premier_LR1 [ NonTerminal 'U' ] grammaire2 = [ 'a' ]);
-  assert (premier_LR1 [ NonTerminal 'V' ] grammaire2 = [ 'c' ]);
-  assert (premier_LR1 [ NonTerminal 'S' ] grammaire2 = [ 'a'; 'c' ])
+  assert (premier_LL1 [ Terminal 'a'; Terminal 'b' ] grammaire2 = [ 'a' ]);
+  assert (premier_LL1 [ Terminal 'c'; Terminal 'd' ] grammaire2 = [ 'c' ]);
+  assert (premier_LL1 [ NonTerminal 'U' ] grammaire2 = [ 'a' ]);
+  assert (premier_LL1 [ NonTerminal 'V' ] grammaire2 = [ 'c' ]);
+  assert (premier_LL1 [ NonTerminal 'S' ] grammaire2 = [ 'a'; 'c' ])
 
 (*
 let parse (g : ('token_type, 'non_terminal) grammar)
