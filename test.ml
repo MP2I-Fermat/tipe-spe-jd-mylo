743fb62dde00e3360eb8f9c20b8a12d77c961867
Mylo Fawcett <mylo.fawcett@gmail.com>
2025-11-05 19:44:53 +0100

    Enhance working example

--- test.ml
+++ test.ml
@@ -1,3 +1,17 @@
 let hello_world () = print_endline "Hello, World!";;
 
 hello_world ()
+
+let x = 0
+let rec sum (l : int list) = match l with [] -> 0 | x :: q -> x + sum q
+
+let sum_tr (l : int list) =
+  let rec sum_aux (l : int list) (s : int) =
+    match l with [] -> s | x :: q -> sum_aux q (x + s)
+  in
+  sum_aux l 0
+
+let _ =
+  let l = [ 0; 1; 2; 3; 4; 5 ] in
+  print_endline (string_of_int (sum l));
+  print_endline (string_of_int (sum_tr l))
