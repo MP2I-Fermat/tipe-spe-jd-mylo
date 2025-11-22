3d1f8de3412428a55195826c698f6a8359456f6f
Mylo Fawcett <mylo.fawcett@gmail.com>
2025-11-22 15:41:19 +0100

    Implement rectification

--- src/rectify.ml
+++ src/rectify.ml
@@ -131,8 +131,208 @@ let is_rectify_candidate (f : function_) ~(binding_is_rec : bool) : bool =
   && contains_only_full_applications f f.body
   && List.for_all is_variable_pattern f.parameters
 
-let rectify ({ name; parameters; body } as f : function_) : function_ =
-  let aux_name = name ^ "_aux" in
+let is_simple_expression (e : expression) : bool =
+  let res = ref true in
+  let _ =
+    map_expression
+      (fun e' ->
+        (res := e == e' || match e' with Variable _ -> true | _ -> false);
+        e')
+      e ~direction:Inwards
+  in
+  !res
+
+let linearize (e : expression) : (pattern * expression) list * string =
+  let var_name (count : int) : string = "_lin_" ^ string_of_int count in
+
+  let rec linearize_items_from (e : expression) (count : int) :
+      int * (pattern * expression) list * string =
+    match e with
+    | Variable v -> (count, [], v)
+    | Match { value; cases } ->
+        let count, value_items, value_name = linearize_items_from value count in
+        let match_name = var_name count in
+        ( count + 1,
+          value_items
+          @ [ (Ident match_name, Match { value = Variable value_name; cases }) ],
+          match_name )
+    | InfixOperation { lhs; operation; rhs } ->
+        let count, lhs_items, lhs_name = linearize_items_from lhs count in
+        let count, rhs_items, rhs_name = linearize_items_from rhs count in
+        let op_name = var_name count in
+        ( count + 1,
+          lhs_items @ rhs_items
+          @ [
+              ( Ident op_name,
+                InfixOperation
+                  {
+                    lhs = Variable lhs_name;
+                    operation;
+                    rhs = Variable rhs_name;
+                  } );
+            ],
+          op_name )
+    | Constant c ->
+        let constant_name = var_name count in
+        (count + 1, [ (Ident constant_name, Constant c) ], constant_name)
+    | Parenthesised { inner } -> linearize_items_from inner count
+    | TypeCoercion { inner; typ } ->
+        let count, inner_items, inner_name = linearize_items_from inner count in
+        let coercion_name = var_name count in
+        ( count + 1,
+          inner_items
+          @ [
+              ( Ident coercion_name,
+                TypeCoercion { inner = Variable inner_name; typ } );
+            ],
+          coercion_name )
+    | ListLiteral _ -> failwith "Unimplemented: linearize ListLiteral"
+    | ArrayLiteral _ -> failwith "Unimplemented: linearize ArrayLiteral"
+    | RecordLiteral _ -> failwith "Unimplemented: linearize RecordLiteral"
+    | WhileLoop _ -> failwith "Unimplemented: linearize WhileLoop"
+    | ForLoop _ -> failwith "Unimplemented: linearize ForLoop"
+    | Dereference _ -> failwith "Unimplemented: linearize Dereference"
+    | FieldAccess _ -> failwith "Unimplemented: linearize FieldAccess"
+    | ArrayAccess _ -> failwith "Unimplemented: linearize ArrayAccess"
+    | FunctionApplication { receiver; arguments } ->
+        let count, receiver_items, receiver_name =
+          linearize_items_from receiver count
+        in
+        let count, arguments_items, arguments_name =
+          List.fold_left
+            (fun (count, arguments_items, arguments_names) argument ->
+              let count, argument_items, argument_name =
+                linearize_items_from argument count
+              in
+              ( count,
+                arguments_items @ argument_items,
+                arguments_names @ [ argument_name ] ))
+            (count, [], []) arguments
+        in
+        let application_name = var_name count in
+        ( count + 1,
+          receiver_items @ arguments_items
+          @ [
+              ( Ident application_name,
+                FunctionApplication
+                  {
+                    receiver = Variable receiver_name;
+                    arguments =
+                      List.map (fun v -> Caml_light.Variable v) arguments_name;
+                  } );
+            ],
+          application_name )
+    | PrefixOperation _ -> failwith "Unimplemented: linearize PrefixOperation"
+    | Negation _ -> failwith "Unimplemented: linearize Negation"
+    | Tuple _ -> failwith "Unimplemented: linearize Tuple"
+    | FieldAssignment _ -> failwith "Unimplemented: linearize FieldAssignment"
+    | ArrayAssignment _ -> failwith "Unimplemented: linearize ArrayAssignment"
+    | ReferenceAssignment _ ->
+        failwith "Unimplemented: linearize ReferenceAssignment"
+    | If _ -> failwith "Unimplemented: linearize If"
+    | Sequence _ -> failwith "Unimplemented: linearize Sequence"
+    | Try _ -> failwith "Unimplemented: linearize Try"
+    | FunctionLiteral { style; cases } ->
+        let function_name = var_name count in
+        ( count + 1,
+          [ (Ident function_name, FunctionLiteral { style; cases }) ],
+          function_name )
+    | LetBinding { bindings; is_rec; inner } ->
+        let count, bindings_items =
+          List.fold_left
+            (fun (count, bindings_items) binding ->
+              match binding with
+              | (Caml_light.Variable { lhs; value } : binding) ->
+                  if is_simple_expression value then
+                    (count, bindings_items @ [ (lhs, value) ])
+                  else
+                    let count, value_items, value_name =
+                      linearize_items_from value count
+                    in
+                    ( count,
+                      bindings_items @ value_items
+                      @ [ (lhs, Variable value_name) ] )
+              | Function { name; parameters; body } ->
+                  ( count,
+                    [
+                      ( Ident name,
+                        FunctionLiteral
+                          { style = Fun; cases = [ (parameters, body) ] } );
+                    ] ))
+            (count, []) bindings
+        in
+        let count, inner_items, inner_name = linearize_items_from inner count in
+        (count, bindings_items @ inner_items, inner_name)
+    | StringAccess _ -> failwith "Unimplemented: linearize StringAccess"
+    | StringAssignment _ -> failwith "Unimplemented: linearize StringAssignment"
+  in
+  let count, items, root = linearize_items_from e 0 in
+  (items, root)
+
+let rec delinearize ((items, root) : (pattern * expression) list * string) :
+    expression =
+  let collapsed_items =
+    List.rev
+      (List.fold_left
+         (fun so_far (pattern, def) ->
+           let substituted = ref [] in
+           let linearized_def =
+             map_expression ~direction:Outwards
+               (fun e ->
+                 match e with
+                 | Variable v
+                   when String.starts_with ~prefix:"_lin_" v
+                        || v = "_aux_continue" || v = "_new_aux_continue" -> (
+                     match List.assoc_opt (Ident v) so_far with
+                     | Some def ->
+                         substituted := v :: !substituted;
+                         Parenthesised { inner = def; style = Parenthesis }
+                     | None -> Variable v)
+                 | _ -> e)
+               def
+           in
+           let filtered_decls =
+             List.filter
+               (fun (pattern, _) ->
+                 match pattern with
+                 | Ident v -> not (List.mem v !substituted)
+                 | _ -> true)
+               so_far
+           in
+           (pattern, linearized_def) :: filtered_decls)
+         [] items)
+  in
+
+  let rec delinearize_items (items : (pattern * expression) list)
+      (root : expression) : expression =
+    match items with
+    | (pattern, def) :: q -> (
+        let q_expression = delinearize_items q root in
+        match q_expression with
+        | Variable v when Ident v = pattern -> def
+        | _ ->
+            let is_rec =
+              match pattern with
+              | Ident v -> (
+                  match def with
+                  | FunctionLiteral _ -> contains_reference v def
+                  | _ -> false)
+              | _ -> false
+            in
+            LetBinding
+              {
+                is_rec;
+                bindings = [ Variable { lhs = pattern; value = def } ];
+                inner = q_expression;
+              })
+    | [] -> root
+  in
+
+  delinearize_items collapsed_items (Variable root)
+
+let rectify ({ name = function_name; parameters; body } : function_) : function_
+    =
+  let aux_name = function_name ^ "_aux" in
 
   let id_function : expression =
     Parenthesised
@@ -147,23 +347,68 @@ let rectify ({ name; parameters; body } as f : function_) : function_ =
       }
   in
 
-  let rec replace_invocations (f : function_) (e : expression) : expression =
-    map_expression ~direction:Outwards
-      (fun e ->
-        match e with
-        | FunctionApplication { receiver = Variable v; arguments }
-          when v = f.name ->
-            FunctionApplication
-              {
-                receiver = Variable aux_name;
-                arguments = arguments @ [ id_function ];
-              }
-        | _ -> e)
-      e
+  let rec rectify_linear
+      ((items, root) : (pattern * expression) list * variable) :
+      (pattern * expression) list * variable =
+    match items with
+    | [] ->
+        ( [
+            ( Ident "_lin_ret",
+              FunctionApplication
+                {
+                  receiver = Variable "_aux_continue";
+                  arguments = [ Variable root ];
+                } );
+          ],
+          "_lin_ret" )
+    | (name, value) :: q ->
+        if contains_reference function_name value then
+          if q <> [] || Ident root <> name then
+            let new_continuation_body =
+              rectify_expression (delinearize (q, root))
+            in
+            ( [
+                ( Ident "_new_aux_continue",
+                  FunctionLiteral
+                    {
+                      style = Fun;
+                      cases = [ ([ name ], new_continuation_body) ];
+                    } );
+                (Ident "_aux_continue", Variable "_new_aux_continue");
+                (Ident "_lin_ret", push_rectification_down value);
+              ],
+              "_lin_ret" )
+          else
+            ([ (Ident "_lin_ret", push_rectification_down value) ], "_lin_ret")
+        else
+          let inner_items, inner_root = rectify_linear (q, root) in
+          ((name, value) :: inner_items, inner_root)
+  and rectify_expression (e : expression) : expression =
+    delinearize (rectify_linear (linearize e))
+  and push_rectification_down (e : expression) : expression =
+    match e with
+    | Match { value; cases } ->
+        Match
+          {
+            value;
+            cases =
+              List.map
+                (fun (patterns, value) -> (patterns, rectify_expression value))
+                cases;
+          }
+    | FunctionApplication
+        { receiver = Caml_light.Variable receiver_name; arguments }
+      when receiver_name = function_name ->
+        FunctionApplication
+          {
+            receiver = Variable aux_name;
+            arguments = arguments @ [ Variable "_aux_continue" ];
+          }
+    | _ -> e
   in
 
   {
-    name;
+    name = function_name;
     parameters;
     body =
       LetBinding
@@ -179,7 +424,7 @@ let rectify ({ name; parameters; body } as f : function_) : function_ =
                         Parenthesised
                           (TypeCoercion
                              {
-                               inner = Ident "aux_continue";
+                               inner = Ident "_aux_continue";
                                typ =
                                  Function
                                    {
@@ -188,19 +433,7 @@ let rectify ({ name; parameters; body } as f : function_) : function_ =
                                    };
                              });
                       ];
-                  body =
-                    FunctionApplication
-                      {
-                        receiver = Variable "aux_continue";
-                        arguments =
-                          [
-                            Parenthesised
-                              {
-                                style = Parenthesis;
-                                inner = replace_invocations f body;
-                              };
-                          ];
-                      };
+                  body = rectify_expression body;
                 };
             ];
           is_rec = true;
@@ -218,7 +451,20 @@ let rectify ({ name; parameters; body } as f : function_) : function_ =
   }
 
 let rectify_program (p : program) : program =
-  let rec rectify_expression (e : expression) : expression = e
+  let rec rectify_expression (e : expression) : expression =
+    map_expression
+      (fun e ->
+        match e with
+        | LetBinding { bindings; is_rec; inner } ->
+            LetBinding
+              {
+                bindings =
+                  List.map (rectify_binding ~binding_is_rec:is_rec) bindings;
+                is_rec;
+                inner;
+              }
+        | _ -> e)
+      e ~direction:Outwards
   and rectify_binding (b : binding) ~(binding_is_rec : bool) : binding =
     match b with
     | Variable { lhs; value } ->
