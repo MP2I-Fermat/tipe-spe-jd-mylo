f1a978c083864dcb3c0c9604e0026ef30b9147d4
Mylo Fawcett <mylo.fawcett@gmail.com>
2025-11-22 15:41:19 +0100

    Implement rectification

--- test.ml
+++ test.ml
@@ -1,3 +1,13 @@
 let rec sum (l : int list) = match l with [] -> 0 | x :: q -> x + sum q;;
 
 print_endline (string_of_int (sum [ 1; 2; 3 ]))
+
+type int_tree = Leaf of int | Node of int_tree * int * int_tree
+
+let rec sum_tree (t : int_tree) =
+  match t with
+  | Leaf v -> v
+  | Node (left, v, right) -> sum_tree left + v + sum_tree right
+;;
+
+print_endline (string_of_int (sum_tree (Node (Leaf 1, 2, Leaf 3))))
