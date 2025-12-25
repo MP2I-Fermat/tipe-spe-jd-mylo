2217cb8980427de395ab349a36274e584803ea59
Mylo Fawcett <mylo.fawcett@gmail.com>
2025-12-25 19:04:19 +0000

    Linearize (as per theory) and delinearize (not yet formalized)

--- src/rectify_test.ml
+++ src/rectify_test.ml
@@ -10,6 +10,36 @@ let test_source =
   test_source
 
 let program = parse_caml_light_ast test_source
-let rectified = rectify_program program;;
+
+let rectified =
+  program
+  |> List.map (fun phrase ->
+         match phrase with
+         | ValueDefinition { bindings; is_rec } ->
+             ValueDefinition
+               {
+                 is_rec;
+                 bindings =
+                   bindings
+                   |> List.map (fun (binding : binding) ->
+                          match binding with
+                          | Variable { lhs; value } ->
+                              (Variable
+                                 {
+                                   lhs;
+                                   value = delinearize (fst (linearize value 0));
+                                 }
+                                : binding)
+                          | Function { name; parameters; body } ->
+                              Function
+                                {
+                                  name;
+                                  parameters;
+                                  body = delinearize (fst (linearize body 0));
+                                });
+               }
+         | Expression e -> Expression (delinearize (fst (linearize e 0)))
+         | _ -> phrase)
+;;
 
 print_endline (string_of_ast rectified)
