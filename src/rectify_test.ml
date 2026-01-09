d4d00544e21372d4d9ea6bccad9be0ab7096194a
Mylo Fawcett <mylo.fawcett@gmail.com>
2026-01-09 21:30:07 +0100

    Standalone rectification

--- src/rectify_test.ml
+++ src/rectify_test.ml
@@ -11,34 +11,133 @@ let test_source =
 
 let program = parse_caml_light_ast test_source
 
+let rec get_name (p : pattern) : variable option =
+  match p with
+  | Ident n -> Some n
+  | Underscore -> None
+  | Parenthesised inner -> get_name inner
+  | TypeCoercion { inner } -> get_name inner
+  | Constant _ -> None
+  | Record _ -> None
+  | List _ -> None
+  | Construction _ -> None
+  | Concatenation _ -> None
+  | Tuple _ -> None
+  | Or _ -> None
+  | As { name } -> Some name
+
 let rectified =
   program
   |> List.map (fun phrase ->
          match phrase with
-         | ValueDefinition { bindings; is_rec } ->
-             ValueDefinition
-               {
-                 is_rec;
-                 bindings =
-                   bindings
-                   |> List.map (fun (binding : binding) ->
-                          match binding with
-                          | Variable { lhs; value } ->
-                              (Variable
-                                 {
-                                   lhs;
-                                   value = delinearize (fst (linearize value 0));
-                                 }
-                                : binding)
-                          | Function { name; parameters; body } ->
-                              Function
+         | ValueDefinition { bindings; is_rec } -> (
+             let k = ref 0 in
+             let defined_functions =
+               bindings
+               |> List.filter_map (fun (binding : binding) ->
+                      match binding with
+                      | Variable _ -> None
+                      | Function { name; parameters; body } ->
+                          let body_lin, k' = linearize body !k in
+                          k := k';
+                          Some
+                            ( name,
+                              FunctionLiteral
                                 {
-                                  name;
-                                  parameters;
-                                  body = delinearize (fst (linearize body 0));
-                                });
-               }
-         | Expression e -> Expression (delinearize (fst (linearize e 0)))
+                                  style = Fun;
+                                  cases = [ (parameters, body_lin) ];
+                                } ))
+             in
+
+             let new_name (n : string) = n ^ "_new" in
+
+             match cloture_rectifiable defined_functions with
+             (* TODO: Dig down? *)
+             | None -> phrase
+             | Some clot ->
+                 ValueDefinition
+                   {
+                     is_rec;
+                     bindings =
+                       bindings
+                       |> List.map (fun (binding : binding) ->
+                              match binding with
+                              | Variable _ -> [ binding ]
+                              | Function { name; parameters; body } ->
+                                  let linearized_body =
+                                    match List.assoc name defined_functions with
+                                    | FunctionLiteral
+                                        { cases = [ (_, body_lin) ] } ->
+                                        body_lin
+                                    | _ ->
+                                        failwith
+                                          "Didn't find expected definition"
+                                  in
+
+                                  let new_fn =
+                                    (Function
+                                       {
+                                         name = new_name name;
+                                         parameters =
+                                           parameters @ [ Ident "cont" ];
+                                         body =
+                                           delinearize
+                                             (push_new_names
+                                                (rectify linearized_body clot
+                                                   "cont")
+                                                clot "cont" new_name);
+                                       }
+                                      : Caml_light.binding)
+                                  in
+
+                                  let interceptor =
+                                    (Function
+                                       {
+                                         name;
+                                         parameters =
+                                           List.init (List.length parameters)
+                                             (fun i ->
+                                               Ident ("arg" ^ string_of_int i));
+                                         body =
+                                           FunctionApplication
+                                             {
+                                               receiver =
+                                                 Variable (new_name name);
+                                               arguments =
+                                                 List.init
+                                                   (List.length parameters)
+                                                   (fun i ->
+                                                     (Variable
+                                                        ("arg" ^ string_of_int i)
+                                                       : Caml_light.expression))
+                                                 @ [
+                                                     Parenthesised
+                                                       {
+                                                         style = Parenthesis;
+                                                         inner =
+                                                           FunctionLiteral
+                                                             {
+                                                               style = Fun;
+                                                               cases =
+                                                                 [
+                                                                   ( [
+                                                                       Ident
+                                                                         "res";
+                                                                     ],
+                                                                     Variable
+                                                                       "res" );
+                                                                 ];
+                                                             };
+                                                       };
+                                                   ];
+                                             };
+                                       }
+                                      : Caml_light.binding)
+                                  in
+
+                                  [ new_fn; interceptor ])
+                       |> List.concat;
+                   })
          | _ -> phrase)
 ;;
 
