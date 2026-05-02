0b81091b6af9980d47c60f50fe59b72092ce6019
Jean Dubois <jd-dev@laposte.net>
2026-05-02 20:43:10 +0100

    tired of writting that command, putting it in a file

---
+++ src/jdcommand.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+ocamlopt -g utils.ml automaton.ml regex.ml lexer.ml parser.ml regex_grammar.ml grammar_grammar.ml caml_light.ml rectify.ml rectify_helper.ml while.ml while_cli.ml && ./a.out ../test2.1.ml | ocamlformat --name="test2.1g.ml" -
