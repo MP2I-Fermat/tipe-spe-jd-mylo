49d626026e20713a469cafbb1869b5e8f60b2f2c
Jean Dubois <jd-dev@laposte.net>
2026-05-11 10:38:43 +0100

    tweaks

--- oral/jd/diapo/build_and_clean.sh
+++ oral/jd/diapo/build_and_clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-pdflatex diapo.tex && rm *.aux *.log *.nav *.out *.snm *.toc && xdg-open diapo.pdf
+pdflatex diapo.tex && pdflatex diapo.tex && rm *.aux *.log *.nav *.out *.snm *.toc && xdg-open diapo.pdf
 
